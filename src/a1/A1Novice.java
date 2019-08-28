1b8846405fd25ff51bdabdeda84cd735f90ff2db
offic <offic@DESKTOP-AND0C8S.wireless-1x.unc.edu>
2019-08-28 17:06:27 -0400

    after office hours thanks!

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,99 +5,90 @@ import java.util.Scanner;
 
 public class A1Novice 
 {
+	static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) 
 	{
 		//Establishes scanner to collect input
-		Scanner scan = new Scanner(System.in);
 		
 		// Collects number of users
 		int count = scan.nextInt();
 		
 		//Set values of GetInfo to customer_info array
-		String[] dem_info = GetDemographicInfo(count);
-		int[] item_info = GetItemInfo(count);
-		double[] total_prices = GetItems(item_info);
+		//String[] dem_info = GetDemographicInfo(count);
+		//int[] item_info = GetItemInfo(count);
+		//double[] total_prices = GetItems(item_info);
+		//PrintValues(count, total_prices, dem_info);
 		
+		//make huge for loop and put everything there, iteration each person
+		for (int i = count; i > 0; i--) 
+		{
+			String dem_info = GetDemographicInfo(count);
+			int item_info = GetItemInfo(count);
+			double total_price = GetItemPrice(item_info);
+			System.out.println(dem_info + total_price);
+		}
 	}
 	
-	public static String[] GetDemographicInfo(int count) 
-	{
-		Scanner scan = new Scanner(System.in);
-		
+	public static String GetDemographicInfo(int count) 
+	{		
 		String first_name;
 		String last_name; 
 		char first_initial;
 		
 		// Creates array of all customer info based on input
-		String dem_info[] = new String[count];
+		// String dem_info[] = new String[count];
 
 		// Places full name and number of items into array
-		for (int i = 0; i < dem_info.length; i++) 
-		{
+		//for (int i = 0; i < dem_info.length; i++) 
+		//{
 			// Collect first and last name
-			first_name = scan.next();
-			last_name = scan.next();
+		first_name = scan.next();
+		last_name = scan.next();
 			
 			// Collects first initial
-			first_initial = first_name.charAt(0);
+		first_initial = first_name.charAt(0);
 
 			// Place the items into an Array
-			dem_info[i] = first_initial  + ". " + last_name + ": ";
+		String dem_info = first_initial  + ". " + last_name + ": ";
 
-		}
+		//}
 		// Close scanner
-		scan.close();
 		return dem_info;
 	}
 	
-	public static int[] GetItemInfo(int count) 
+	public static int GetItemInfo(int count) 
 	{
-		
-		Scanner scan = new Scanner(System.in);
-		
+				
 		int items;
 		
-		int item_info[] = new int[count];
+		//int item_info[] = new int[count];
 		
-		for (int i = 0; i < item_info.length; i++) 
-		{
-			items = scan.nextInt();
-			
-			item_info[i] = items;
-			
-		}
-		scan.close();
-		return item_info;
-	}
+		items = scan.nextInt();
+						
+		return items;
+}
 
-	public static double[] GetItems(int item_info[]) 
+	public static double GetItemPrice(int items) 
 	{
-		double total_prices[] = new double[item_info.length];
+		//double total_prices[] = new double[item_info.length];
 		
 		double total_price = 0.0;
 
-		//Establishes scanner to collect input
-		Scanner scan = new Scanner(System.in);
-				
-		//Collects information regarding all the items purchased
-		for (int i = 0; i < total_prices.length; i++) 
-		{			
-			int num_items = item_info[i];
+		//Collects information regarding all the items purchased			
 			
-			for (int c = num_items; c > 0; num_items --) 
-			{
-				
+		for (int c = items; c > 0; c --) 
+			
+		{
 				int quantity = scan.nextInt();
 				String item_name = scan.next();
 				double item_price = scan.nextDouble();
 				
 				total_price = (quantity * item_price) + total_price;
 				
-				total_prices[c] = total_price;
-			}
 		}
-		scan.close();
-		return total_prices;
+		return total_price;
+		
 	}
 		
 	public static void PrintValues(int count, double[] total_prices, String[] dem_info) {
@@ -111,31 +102,15 @@ public class A1Novice
 			String print_price = String.format("%.2f", total_price);
 			
 			// get dem info
-			String print_info = dem_info[]
+			String print_info = dem_info[i];
 			
-			// some print statement here
+			System.out.println(print_info + print_price);
+			
 		}
 		
-		
-	}
-	public static String PrintValue1s(int count, double[] total_prices, String[] dem_info, int item_info[])
-	{
-		for (int i = 0; i < count; i++) 
-		{
-			String.format("%.2f", total_price);
-			String dem = dem_info[i];
-			String item = item_info[i];
-			
-			String total_output = dem_info + 
-
-		
-		String total_output = first_initial + last_name + total_price;
-		System.out.println(total_output);
-		return total_output;
-		}
-
 		
 	}
 }
+
 
 		
