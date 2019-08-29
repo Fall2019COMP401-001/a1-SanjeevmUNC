889241b618224acbebca1308a4776c32ea48564e
offic <offic@DESKTOP-AND0C8S.wireless-1x.unc.edu>
2019-08-29 17:56:16 -0400

    WE DONE!

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -2,49 +2,13 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Jedi 
+public class A1Jedi
 {
-	//Establish a scanner to obtain input
+	//Establish scanner to collect input
 	static Scanner scan = new Scanner(System.in);
 	
-	public static void main(String[] args) 
-	{
-		// Get the number of items that are present in the store
-		int total_store_items = scan.nextInt();
-				
-		// Calls function that gets the name of the items in store
-		String[] name_of_all_items = new String[total_store_items];
-		double[] price_of_all_items = new double[total_store_items];
-				
-		// Creates catalog for all the names and prices paired
-		for (int i = 0; i < total_store_items; i++)
-		{
-			String item_name = scan.next();
-					
-			// Place it in the respective array box
-			name_of_all_items[i] = item_name;
-					
-			// Obtain the item price
-			double item_price = scan.nextDouble();
-							
-			// Place it in the respective array box
-			price_of_all_items[i] = item_price;
-		}
-
-		// Gets the amount of customers
-		int total_customers = scan.nextInt();	
-					
-		String[] full_names = new String[total_customers];
-			
-		int[] num_per_item = new int[total_store_items];
-
-		for (int i = 0; i < total_customers; i++) 
-			{
-				full_names[i] = GetPersonName();
-				num_per_item[i] = GetTotalItems(name_of_all_items, total_customers);			
-			}
-	}
 	public static String GetPersonName() {
+
 		
 		// obtain first name
 		String first_name = scan.next();
@@ -58,35 +22,83 @@ public class A1Jedi
 		return full_name;
 		
 	}
+	public static void main(String[] args)
+	{
+		// Get number of items present in store
+		int total_store_items = scan.nextInt();
 
-	public static int GetTotalItems(String[] name_of_all_items, int total_customers) 
-	{		
-		int num_per_item_per_customer = 0; 
+		// Creates arrays that hold the name and price of all items
+		// This array is sized based upon the number of total items
+		String[] name_of_all_items = new String[total_store_items];
+		double[] price_of_all_items = new double[total_store_items];
 		
-		//get num of items bought
-		int num_of_items = scan.nextInt();
-				
-		for (int c = 0; c < num_of_items; c++)
+		// Collect and place all names and prices into the catalog arrays
+		for (int i = 0; i < total_store_items; i++)
 		{
-			// collect quantity of items
-			int quantity = scan.nextInt();
-			
-			// collect name of item
+			// Gets item name
 			String item_name = scan.next();
+					
+			// Place it in the respective array box
+			name_of_all_items[i] = item_name;
+					
+			// Gets item price
+			double item_price = scan.nextDouble();
+							
+			// Place it in the respective array box
+			price_of_all_items[i] = item_price;
+		}
+		
+		//Collect the amount of customers
+		int total_customers = scan.nextInt();
+		
+		// Takes in all the names and puts them into this array
+		String[] full_names = new String[total_customers];
+		int[] num_of_items = new int[total_store_items];
+		
+		int quantity_of_item;
+		int index;
+		int[] num_of_people_bought_x_item = new int[total_store_items];
+		
+		for (int i = 0; i < total_customers; i++)
+		{
+			// Placed names into array 
+			full_names[i] = GetPersonName();
 			
-			// finds the index of the item name that has the same index as the item		
-			int index = 0;
+			// Get number of items they bought
+			int num_of_items_customer_bought = scan.nextInt();
 			
-			for (int z = 0; z < name_of_all_items.length; z++)
+			
+			for (int c = 0; c < num_of_items_customer_bought; c++)
 			{
-				if (item_name.equals(name_of_all_items[z]))
+				// Quantity of items here
+				quantity_of_item = scan.nextInt();
+				
+				String item_name = scan.next();
+				
+				for (int z = 0; z < name_of_all_items.length; z++)
 				{
-					index = z;
-					num_per_item_per_customer = quantity + num_per_item_per_customer;
-				}
+					if (item_name.equals(name_of_all_items[z]))
+					{
+						index = z;
+						num_of_items[z] = num_of_items[z] + quantity_of_item;
+						num_of_people_bought_x_item[z] = num_of_people_bought_x_item[z] + 1;
+					}
+				
+				}		
+			}
+		
+		}
+		for (int i = 0; i < total_store_items; i++)
+		{
+			if (num_of_items[i] == 0)
+			{
+				System.out.println("No customers bought " + name_of_all_items[i]);
+			}
+			else 
+			{
+				System.out.println(num_of_people_bought_x_item[i] + " customers bought " + num_of_items[i] + " " + name_of_all_items[i]);
 			}
 		}
-		return num_per_item_per_customer;
 	}
 	
 }
